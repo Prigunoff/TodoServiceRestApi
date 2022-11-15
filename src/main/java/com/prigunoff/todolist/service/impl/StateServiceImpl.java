9ce7082652e29808ffbb44ee6b02fd9dc30b5a7b
Даниил Прыгунов <idanil430@gmail.com>
2022-11-15 16:16:39 +0200

    Developed StateServiceImpl

---
+++ src/main/java/com/prigunoff/todolist/service/impl/StateServiceImpl.java
@@ -0,0 +1,52 @@
+package com.prigunoff.todolist.service.impl;
+
+import com.prigunoff.todolist.exceptions.NullEntityReferenceException;
+import com.prigunoff.todolist.model.State;
+import com.prigunoff.todolist.repository.StateRepository;
+import com.prigunoff.todolist.service.StateService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class StateServiceImpl implements StateService {
+    private StateRepository stateRepository;
+
+    public StateServiceImpl(StateRepository stateRepository) {
+        this.stateRepository = stateRepository;
+    }
+
+    @Override
+    public State create(State state) {
+        return stateRepository.save(state);
+    }
+
+    @Override
+    public State readById(long id) {
+        return stateRepository.findById(id)
+                .orElseThrow(() -> new NullEntityReferenceException("State with id " + id + " not found"));
+    }
+
+    @Override
+    public State update(State state) {
+        return stateRepository.save(state);
+    }
+
+    @Override
+    public void delete(long id) {
+        State state = readById(id);
+        stateRepository.delete(state);
+    }
+
+    @Override
+    public State getByName(String name) {
+        Optional<State> optional = Optional.ofNullable(stateRepository.getByName(name));
+        return optional.get();
+    }
+
+    @Override
+    public List<State> getAll() {
+        List<State> states = stateRepository.getAll();
+        return states.isEmpty() ? new ArrayList<>() : states;
+    }
+}
