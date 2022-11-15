3ca023681105e24b113260af685ca5c0388a8c74
Даниил Прыгунов <idanil430@gmail.com>
2022-11-15 16:16:47 +0200

    Developed RoleServiceImpl

---
+++ src/main/java/com/prigunoff/todolist/service/impl/RoleServiceImpl.java
@@ -0,0 +1,46 @@
+package com.prigunoff.todolist.service.impl;
+
+import com.prigunoff.todolist.exceptions.NullEntityReferenceException;
+import com.prigunoff.todolist.model.Role;
+import com.prigunoff.todolist.repository.RoleRepository;
+import com.prigunoff.todolist.service.RoleService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RoleServiceImpl implements RoleService {
+    private RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role create(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role readById(long id) {
+        Optional<Role> optional = roleRepository.findById(id);
+        return roleRepository.findById(id).orElseThrow(() -> new NullEntityReferenceException("Role with id" + id + "not found"));
+    }
+
+    @Override
+    public Role update(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void delete(long id) {
+        Role role = readById(id);
+        roleRepository.delete(role);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        List<Role> roles = roleRepository.findAll();
+        return roles.isEmpty() ? new ArrayList<>() : roles;
+    }
+}
