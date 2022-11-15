1be09b4ba74d9d9bf1c96683f3961300e7dd4d03
Даниил Прыгунов <idanil430@gmail.com>
2022-11-15 14:37:09 +0200

    Developed TodoRepository

---
+++ src/main/java/com/prigunoff/todolist/repository/ToDoRepository.java
@@ -0,0 +1,15 @@
+package com.prigunoff.todolist.repository;
+
+import com.prigunoff.todolist.model.ToDo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ToDoRepository {
+    @Query(value = "select id, title, created_at, owner_id from todos where owner_id = ?1 union " +
+            "select id, title, created_at, owner_id from todos inner join todo_collaborator on id = todo_id and " +
+            "collaborator_id = ?1", nativeQuery = true)
+    List<ToDo> getByUserId(long userId);
+}
